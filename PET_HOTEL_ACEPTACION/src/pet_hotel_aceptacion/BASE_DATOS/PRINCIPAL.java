191444aa4607069cf4c274963d4f4e5fa7f0cc6e
Ángel Hernán Alberto Patricio Arroyo <129218946+AngelHer2005@users.noreply.github.com>
2024-04-30 17:11:52 -0500

    Add files via upload

---
+++ PET_HOTEL_ACEPTACION/src/pet_hotel_aceptacion/BASE_DATOS/PRINCIPAL.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pet_hotel_aceptacion.BASE_DATOS;
+
+/**
+ *
+ * @author Academico
+ */
+public class PRINCIPAL {
+    
+}
